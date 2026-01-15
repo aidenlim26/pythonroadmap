aea16073c3082a25b10f90be9a9e4828f500089d
aidenp <wasdijkljohnchan@gmail.com>
2026-01-15 08:35:13 +0800

    matplotlib

--- scikit-learn/skills/intro.py
+++ scikit-learn/skills/intro.py
@@ -1,5 +1,7 @@
 from sklearn.datasets import load_breast_cancer
 
-data = load_breast_cancer()
+print(load_breast_cancer())
 
-print(data)
+#data = load_breast_cancer(as_frame=True).frame.to_string()          #loads the data as a dataframe
+
+#print(data)
